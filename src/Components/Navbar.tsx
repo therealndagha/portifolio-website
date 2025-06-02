cd5087bf06d73a3f8d70d5ef6bccd36493b36f39
Ndagha Kang'oma <ndaghakangoma@gmail.com>
2025-06-02 15:01:07 +0200

    Add initial responsive navbar code

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,8 +1,46 @@
-
+import { navLinks } from "@/utilities/NavLinks";
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
+import { FaBars } from "react-icons/fa";
 const Navbar = () =>{
     return (
-        <nav className="">
-              Navbar 
+        <nav className="flex flex-row items-center justify-between space-x-20 mt-10 mr-10 ml-10">
+            <div>
+                <h3 className="logo">Ndagha Kang'oma</h3>
+            </div>
+            <div className="hidden md:block">
+                 <div className="flex flex-row items-center justify-center space-x-10">
+                        {
+                    navLinks.map((singleNavLink)=> (
+                        <div key={singleNavLink.title}>
+                              {
+                                singleNavLink.title === 'Contacts' ? <button  className="bg-some-blue-shade text-white py-5 px-2.5 rounded-md hover:outline-2 hover:outline-some-lighter-blue-shade hover:bg-blue-400 hover:ring-blue-950"><a href={singleNavLink.href}>{singleNavLink.title}</a></button> : <button ><a href={singleNavLink.href}>{singleNavLink.title}</a></button>
+                              }
+                        </div>
+                    ))
+                  }
+                 </div>
+                 
+            </div>
+            <Sheet>
+                <SheetTrigger className="block md:hidden"><FaBars size={24}/></SheetTrigger>
+                <SheetContent side="left">
+                    <SheetHeader>
+                        <SheetTitle className="logo">Ndagha Kang'oma</SheetTitle>
+                    </SheetHeader>
+                    <div className="flex flex-col space-y-5">
+                        {
+                            navLinks.map((singleNavLinks)=>(
+                                <div key={singleNavLinks.title}>
+                                    <button>{singleNavLinks.title}</button>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </SheetContent>
+            </Sheet>
+
+
+
         </nav>
     )
 }
---
+++ src/utilities/NavLinks.ts
@@ -0,0 +1,24 @@
+
+type navLinkType = {
+    title: string,
+    href: string
+}
+
+type navLinksArrayType = navLinkType[];
+
+
+export const navLinks:navLinksArrayType = [
+    {
+        title: 'Services',
+        href: '#services'
+    },
+    {
+        title: 'About',
+        href: '#about',
+
+    },
+    {
+        title: 'Contacts',
+        href: '#contacts'
+    }
+]
