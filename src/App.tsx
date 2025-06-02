713f0950e822c733b9a1b48807a20ea0f6dd4885
Ndagha <ndaghakangoma@gmail.com>
2025-06-02 14:13:25 +0200

    Add initial setup code

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Hero from "./Components/Hero";
+import Navbar from "./Components/Navbar";
+
+
+function App (){
+  return (
+    <main>
+      
+      <Navbar/>
+      <Hero/>  
+    </main>
+  )
+}
+
+export default App;
