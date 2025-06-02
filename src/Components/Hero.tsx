5c529e629c231050ae9630b2a9356964bfa710fb
Ndagha Kang'oma <ndaghakangoma@gmail.com>
2025-06-02 20:26:43 +0200

    Add Hero section

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,8 +1,26 @@
 
 const Hero = () =>{
     return (
-        <div>
-            Hero
+        <div className="flex flex-row items-center justify-center container-hero mt-10 sm:mt-10 md:mt-0">
+            {/**Welcome message */}
+            <div className="flex flex-col space-y-5">
+               <div>
+                 <h3 className="hero-hello-message">Hello there!</h3>
+               </div>
+                <div>
+                    <p className="hero-description-message">I'm Ndagha Kang'oma. <span className="hero-description-message-span">Software Developer</span> based in Lilongwe, Malawi.</p>
+                </div>
+                <div>
+                    <p className="hero-description-message2">I’m passionate about building clean, functional software that solves real-world problems. Currently focused on growing my skills in full-stack development, I enjoy working on meaningful projects and continuously learning new technologies.</p>
+                </div>
+                <div>
+                    <button className="bg-some-blue-shade text-white px-5 py-2.5 hover:bg-some-lighter-blue-shade"><a href="#contacts">Hire Me</a></button>
+                </div>
+            </div>
+            {/**optional image */}
+            <div className="hidden md:block  items-center justify-center">
+                <img src="ndagha-no-bg.png" className="w-80" alt="ndagha" />
+            </div>
         </div>
     )
 }
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -5,7 +5,10 @@ const Navbar = () =>{
     return (
         <nav className="flex flex-row items-center justify-between space-x-20 mt-10 mr-10 ml-10">
             <div>
-                <h3 className="logo">Ndagha Kang'oma</h3>
+                <div className="flex items-center ">
+                    <img src='ndagha.jpg' className="w-5 h-5 rounded-md" alt="ndagha" />
+                    <h3 className="logo">Ndagha Kang'oma</h3>
+                </div>
             </div>
             <div className="hidden md:block">
                  <div className="flex flex-row items-center justify-center space-x-10">
@@ -13,7 +16,7 @@ const Navbar = () =>{
                     navLinks.map((singleNavLink)=> (
                         <div key={singleNavLink.title}>
                               {
-                                singleNavLink.title === 'Contacts' ? <button  className="bg-some-blue-shade text-white py-5 px-2.5 rounded-md hover:outline-2 hover:outline-some-lighter-blue-shade hover:bg-blue-400 hover:ring-blue-950"><a href={singleNavLink.href}>{singleNavLink.title}</a></button> : <button ><a href={singleNavLink.href}>{singleNavLink.title}</a></button>
+                                singleNavLink.title === 'Contacts' ? <button  className="contact_button"><a href={singleNavLink.href} >{singleNavLink.title}</a></button> : <button ><a href={singleNavLink.href} className="navlink">{singleNavLink.title}</a></button>
                               }
                         </div>
                     ))
@@ -25,13 +28,16 @@ const Navbar = () =>{
                 <SheetTrigger className="block md:hidden"><FaBars size={24}/></SheetTrigger>
                 <SheetContent side="left">
                     <SheetHeader>
-                        <SheetTitle className="logo">Ndagha Kang'oma</SheetTitle>
+                             <div className="flex items-center">
+                                    <img src='ndagha.jpg' className="w-5 h-5 rounded-md" alt="ndagha" />
+                                    <h3 className="logo">Ndagha Kang'oma</h3>
+                             </div>
                     </SheetHeader>
-                    <div className="flex flex-col space-y-5">
+                    <div className="flex flex-col space-y-5 p-10">
                         {
                             navLinks.map((singleNavLinks)=>(
                                 <div key={singleNavLinks.title}>
-                                    <button>{singleNavLinks.title}</button>
+                                    <button><a href={singleNavLinks.title} className="navlink">{singleNavLinks.title}</a></button>
                                 </div>
                             ))
                         }
