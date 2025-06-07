6465d14686d927784dc9ce2e1a7e8daada950ac5
Ndagha Kang'oma <ndaghakangoma@gmail.com>
2025-06-07 17:38:24 +0200

    Add bit of spacing between contacts

--- src/Components/Contacts.tsx
+++ src/Components/Contacts.tsx
@@ -2,20 +2,28 @@
 
 const Contacts = () => {
   return (
-    <section className="bg-some-blue-shade text-white font-inter">
+    <section className="bg-some-blue-shade  font-inter text-white">
         <div className="flex flex-col md:flex-row items-center justify-center ">
-             <div className="p-10">
-                  <p>Ndagha Kang'oma</p>
-                  <p>Area 23, Lilongwe, Malawi</p>
-                  <p>+265 99 596 3440</p>
-                  <p>+265 88 787 0954</p>
-                  <p>ndaghakangoma@gmail.com</p>
-                  <p>cis-025-19@must.ac.mw</p>
-             </div>
-             <div className="flex items-center justify-center mb-10">
-                <p className="text-sm">&copy; 2025 Ndagha Kang'oma. All rights reserved.</p>
+             <div className="p-10 space-y-2.5">
+                  <div>
+                        <p>Ndagha Kang'oma</p>
+                        <p >Area 23, Lilongwe, Malawi</p>
+                  </div>
+                  <div>
+                         
+                        <p>+265 99 596 3440</p>
+                        <p>+265 88 787 0954</p>
+                  </div>
+                  <div>
+                         <p>ndaghakangoma@gmail.com</p>
+                         <p>cis-025-19@must.ac.mw</p>
+                  </div>
+                 
              </div>
         </div>
+            <div className="flex items-center justify-center">
+                <p className="text-sm mb-10">&copy; 2025 Ndagha Kang'oma. All rights reserved.</p>
+             </div>
     </section>
   )
 }
